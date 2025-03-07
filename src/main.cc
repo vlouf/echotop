8f999237fceb12698ebc97f6c9deb8bd59113375
Valentin Louf <valentin.louf@bom.gov.au>
2025-03-07 19:17:35 +1100

    working version

--- src/main.cc
+++ src/main.cc
@@ -49,13 +49,12 @@ struct volume{
 };
 
 struct radarset{
-    volume dbzh;
-    vector<float> elevation;
-    string source;
-    string date;
-    string time;
-  };
-
+  volume dbzh;
+  vector<float> elevation;
+  string source;
+  string date;
+  string time;
+};
 
 constexpr auto try_again = "try --help for usage instructions\n";
 constexpr auto usage_string =
@@ -87,219 +86,215 @@ constexpr struct option long_options[] =
 
 template<typename T>
 std::vector<size_t> argsort(const std::vector<T> &array) {
-    std::vector<size_t> indices(array.size());
-    std::iota(indices.begin(), indices.end(), 0);
-    std::sort(indices.begin(), indices.end(),
-              [&array](int left, int right) -> bool {
-                  // sort indices according to corresponding array element
-                  return array[left] < array[right];
-              });
+  std::vector<size_t> indices(array.size());
+  std::iota(indices.begin(), indices.end(), 0);
+  std::sort(indices.begin(), indices.end(),
+            [&array](int left, int right) -> bool {
+                // sort indices according to corresponding array element
+                return array[left] < array[right];
+            });
 
-    return indices;
+  return indices;
 }
 
 auto get_elevation(io::odim::polar_volume const vol_odim) -> vector<float>{
-    const size_t nelev = vol_odim.scan_count();
-    vector<float> elevation;
+  const size_t nelev = vol_odim.scan_count();
+  vector<float> elevation;
 
-    for (size_t iscan = 0; iscan < nelev; ++iscan){
-          auto scan_odim = vol_odim.scan_open(iscan);
-          elevation.push_back(scan_odim.elevation_angle());
-      }
-
-    return elevation;
+  for (size_t iscan = 0; iscan < nelev; ++iscan){
+    auto scan_odim = vol_odim.scan_open(iscan);
+    elevation.push_back(scan_odim.elevation_angle());
   }
+
+  return elevation;
+}
 
 auto read_moment(io::odim::polar_volume const vol_odim, string moment) -> volume{
-    auto vol = volume{};
+  auto vol = volume{};
 
-    vol.location.lat = vol_odim.latitude() * 1_deg;
-    vol.location.lon = vol_odim.longitude() * 1_deg;
-    vol.location.alt = vol_odim.height();
+  vol.location.lat = vol_odim.latitude() * 1_deg;
+  vol.location.lon = vol_odim.longitude() * 1_deg;
+  vol.location.alt = vol_odim.height();
 
-    for (size_t iscan = 0; iscan < vol_odim.scan_count(); ++iscan)
+  for (size_t iscan = 0; iscan < vol_odim.scan_count(); ++iscan){
+    auto scan_odim = vol_odim.scan_open(iscan);
+    auto scan = sweep{};
+
+    scan.beam = radar::beam_propagation{vol.location.alt, scan_odim.elevation_angle() * 1_deg};
+    scan.bins.resize(scan_odim.bin_count());
+
+    auto range_scale = scan_odim.range_scale();
+    auto range_start = scan_odim.range_start() * 1000 + range_scale * 0.5;
+    for (size_t i = 0; i < scan.bins.size(); ++i)
     {
-      auto scan_odim = vol_odim.scan_open(iscan);
-      auto scan = sweep{};
-
-      scan.beam = radar::beam_propagation{vol.location.alt, scan_odim.elevation_angle() * 1_deg};
-      scan.bins.resize(scan_odim.bin_count());
-
-      auto range_scale = scan_odim.range_scale();
-      auto range_start = scan_odim.range_start() * 1000 + range_scale * 0.5;
-      for (size_t i = 0; i < scan.bins.size(); ++i)
-      {
-        scan.bins[i].slant_range = range_start + i * range_scale;
-        std::tie(scan.bins[i].ground_range, scan.bins[i].altitude) = scan.beam.ground_range_altitude(scan.bins[i].slant_range);
-      }
-
-      scan.rays.resize(scan_odim.ray_count());
-      auto ray_scale = 360_deg / scan.rays.size();
-      auto ray_start = scan_odim.ray_start() * 1_deg + ray_scale * 0.5;
-      for (size_t i = 0; i < scan.rays.size(); ++i)
-        scan.rays[i] = ray_start + i * ray_scale;
-
-      for (size_t idata = 0; idata < scan_odim.data_count(); ++idata)
-      {
-        auto data_odim = scan_odim.data_open(idata);
-        if (data_odim.quantity() != moment)
-          continue;
-
-        scan.data.resize(vec2z{(size_t) scan_odim.bin_count(), (size_t) scan_odim.ray_count()});
-        data_odim.read_unpack(scan.data.data(), undetect, nodata);
-        vol.sweeps.push_back(std::move(scan));
-        break;
-      }
+      scan.bins[i].slant_range = range_start + i * range_scale;
+      std::tie(scan.bins[i].ground_range, scan.bins[i].altitude) = scan.beam.ground_range_altitude(scan.bins[i].slant_range);
     }
 
-    return vol;
+    scan.rays.resize(scan_odim.ray_count());
+    auto ray_scale = 360_deg / scan.rays.size();
+    auto ray_start = scan_odim.ray_start() * 1_deg + ray_scale * 0.5;
+    for (size_t i = 0; i < scan.rays.size(); ++i)
+      scan.rays[i] = ray_start + i * ray_scale;
+
+    for (size_t idata = 0; idata < scan_odim.data_count(); ++idata)
+    {
+      auto data_odim = scan_odim.data_open(idata);
+      if (data_odim.quantity() != moment)
+        continue;
+
+      scan.data.resize(vec2z{(size_t) scan_odim.bin_count(), (size_t) scan_odim.ray_count()});
+      data_odim.read_unpack(scan.data.data(), undetect, nodata);
+      vol.sweeps.push_back(std::move(scan));
+      break;
+    }
   }
 
+  return vol;
+}
+
 auto read_volume(const std::filesystem::path& path, const string reflname) -> radarset {
-    radarset dset;
-    io::odim::polar_volume vol_odim{path, io_mode::read_only};
+  radarset dset;
+  io::odim::polar_volume vol_odim{path, io_mode::read_only};
 
-    std::cout << "Reading file " << path << "\n";
-    dset.elevation = get_elevation(vol_odim);
-    std::cout << "Reading Moment " << reflname << "\n";
-    dset.dbzh = read_moment(vol_odim, reflname);
+  std::cout << "Reading file " << path << "\n";
+  dset.elevation = get_elevation(vol_odim);
+  std::cout << "Reading Moment " << reflname << "\n";
+  dset.dbzh = read_moment(vol_odim, reflname);
 
-    const auto& attributes = vol_odim.attributes();
-    dset.source = attributes["source"].get_string();
-    dset.date = attributes["date"].get_string();
-    dset.time = attributes["time"].get_string();
+  const auto& attributes = vol_odim.attributes();
+  dset.source = attributes["source"].get_string();
+  dset.date = attributes["date"].get_string();
+  dset.time = attributes["time"].get_string();
 
-    return dset;
+  return dset;
 }
 
 auto run_search(const array1<bin_info>& bins, float target) -> size_t{
-    size_t left = 0;
-    size_t right = bins.size() - 2;
-    std::cout << "Bin size: " << right << std::endl;
+  size_t left = 0;
+  size_t right = bins.size() - 2;
 
-    while (left < right) {
-        size_t mid = (right + left) / 2;
-        if (bins[mid + 1].ground_range < target) {
-            left = mid + 1;
-        } else {
-            right = mid;
-        }
+  while (left < right) {
+    size_t mid = (right + left) / 2;
+    if (bins[mid + 1].ground_range < target) {
+      left = mid + 1;
+    } else {
+      right = mid;
     }
-    return left;
+  }
+  return left;
 }
 
+auto compute_eth(radarset dset) -> array2f{
+  float eth_thld = 10.;
+  float noise_thld = eth_thld-5.f;
+  auto sorted_index = argsort(dset.elevation);
+  const auto nbins = dset.dbzh.sweeps[sorted_index[0]].bins.size();
+  const auto nrays = dset.dbzh.sweeps[sorted_index[0]].rays.size();
+  auto eth = array2f(vec2z(nbins, nrays));
+  eth.fill(0.f);
+
+  auto range_base = dset.dbzh.sweeps[sorted_index[0]].bins;
+
+  for(size_t k = 0; k < sorted_index.size() - 1; k ++){
+    auto thetab = dset.elevation[sorted_index[k]];  // Ref - following Laks conventions.
+    auto thetaa = dset.elevation[sorted_index[k+1]];  // Iter
+    auto range_ref = dset.dbzh.sweeps[sorted_index[k]].bins;
+    auto range_iter = dset.dbzh.sweeps[sorted_index[k + 1]].bins;
+    auto refl_ref = dset.dbzh.sweeps[sorted_index[k]].data;
+    auto refl_iter = dset.dbzh.sweeps[sorted_index[k + 1]].data;
+
+    for(size_t i = 0; i < range_iter.size(); i ++){
+      auto i_b = run_search(range_ref, range_base[i].ground_range);
+      auto i_a = run_search(range_iter, range_base[i].ground_range);
+
+      for(size_t j = 0; j < nrays; j ++){
+        auto refb = refl_ref[j][i_b];
+        auto refa = refl_iter[j][i_a];
+        if(std::isnan(refa) || (refa < noise_thld)) refa = noise_thld;
+
+        if((refb > eth_thld) && (refa < eth_thld)){
+          if(thetaa < 90){
+            eth[j][i] = (eth_thld - refa) * (thetab - thetaa) / (refb - refa) + thetab;
+          } else {
+            eth[j][i] = thetab + 0.5;
+          }
+        }
+      }
+    }
+  }
+
+  for(size_t i = 0; i < nbins; i ++){
+    for(size_t j = 0; j < nrays; j ++){
+      eth[j][i] = range_base[i].ground_range * sin(M_PI * eth[j][i] / 180.);
+    }
+  }
+
+  // TODO: Correct for earth' sphericity.
+
+  return eth;
+}
 
 void process_file(const std::filesystem::path& path){
-    float eth_thld = 10.;
-    float noise_thld = -5.f;
+  std::cout << "Starting process." << std::endl;
+  auto dset = read_volume(path, "DBZH");
+  auto eth = compute_eth(dset);
 
-    std::cout << "Starting process." << std::endl;
-    auto dset = read_volume(path, "DBZH");
-    auto sorted_index = argsort(dset.elevation);
-    const auto nbins = dset.dbzh.sweeps[sorted_index[0]].bins.size();
-    const auto nrays = dset.dbzh.sweeps[sorted_index[0]].rays.size();
-    auto eth = array2f(vec2z(nbins, nrays));
-    eth.fill(0.f);
+  auto sorted_index = argsort(dset.elevation);
+  const auto nbins = dset.dbzh.sweeps[sorted_index[0]].bins.size();
+  const auto nrays = dset.dbzh.sweeps[sorted_index[0]].rays.size();
+  size_t dims[2] = {nrays, nbins};
 
-    auto range_base = dset.dbzh.sweeps[sorted_index[0]].bins;
+  io::odim::polar_volume vol_odim{path, io_mode::read_write};
+  auto scan_odim = vol_odim.scan_open(sorted_index[0]);
+  auto data = scan_odim.data_append(io::odim::data::data_type::f32, 2, dims);
+  data.write(eth.data());
+  data.set_quantity("ETH");
+  data.set_nodata(-1.);
+  data.set_undetect(-1.);
+  data.set_gain(1);
+  data.set_offset(0);
 
-    for(auto k = 0; k < sorted_index.size() - 1; k ++){
-        auto thetab = dset.elevation[sorted_index[k]];  // Ref - following Laks conventions.
-        auto thetaa = dset.elevation[sorted_index[k+1]];  // Iter
-        auto range_ref = dset.dbzh.sweeps[sorted_index[k]].bins;
-        auto range_iter = dset.dbzh.sweeps[sorted_index[k + 1]].bins;
-        auto refl_ref = dset.dbzh.sweeps[sorted_index[k]].data;
-        auto refl_iter = dset.dbzh.sweeps[sorted_index[k + 1]].data;
-
-        for(auto i = 0; i < nbins; i ++){            
-            auto i_r = run_search(range_ref, range_base[i].ground_range);
-            auto i_i = run_search(range_iter, range_base[i].ground_range);
-            for(auto j = 0; j < nrays; j ++){
-                auto refb = refl_ref[j][i_r];
-                auto refa = refl_iter[j][i_i];
-                if(std::isnan(refa)) refa = noise_thld;
-                if(refb >= eth_thld){
-                    eth[j][i] = thetab + 0.5;
-                    continue;
-                }
-                if((refb > eth_thld) && (refa <= eth_thld)){
-                    if(std::fabs(90. - thetaa ) > 0.1){
-                        eth[j][i] = (eth_thld - refa) * (thetab - thetaa) / (refb - refa) + thetab;
-                    } else {
-                        eth[j][i] = thetab + 0.5;
-                    }
-                }
-            }
-        }
-    }
-
-    size_t dims[2] = {nrays, nbins};
-    io::odim::polar_volume vol_odim{path, io_mode::read_write};
-    auto scan_odim = vol_odim.scan_open(sorted_index[0]);
-    auto data = scan_odim.data_append(
-        io::odim::data::data_type::f32
-      , 2
-      , dims      
-      );
-    data.write(eth.data());
-    data.set_quantity("eth");
-    data.set_nodata(-1.);
-    data.set_undetect(-1.);
-    data.set_gain(1);
-    data.set_offset(0);
-
-    std::cout << "Process done." << std::endl;
+  std::cout << "Process done." << std::endl;
 }
 
-int main(int argc, char* argv[])
-{
-  try
-  {
+int main(int argc, char* argv[]){
+  try{
     // process command line
-    while (true)
-    {
+    while (true){
       int option_index = 0;
       int c = getopt_long(argc, argv, short_options, long_options, &option_index);
       if (c == -1)
         break;
-      switch (c)
-      {
-      case 'h':
-        std::cout << usage_string;
-        return EXIT_SUCCESS;
-      case 'g':
-        std::cout << usage_string;
-        return EXIT_SUCCESS;
-      case 't':
-        trace::set_min_level(from_string<trace::level>(optarg));
-        break;
-      case '?':
-        std::cerr << try_again;
-        return EXIT_FAILURE;
+      switch (c){
+        case 'h':
+          std::cout << usage_string;
+          return EXIT_SUCCESS;
+        case 'g':
+          std::cout << usage_string;
+          return EXIT_SUCCESS;
+        case 't':
+          trace::set_min_level(from_string<trace::level>(optarg));
+          break;
+        case '?':
+          std::cerr << try_again;
+          return EXIT_FAILURE;
       }
     }
 
-    if (argc - optind != 1)
-    {
-      std::cerr << "missing required parameter\n" << try_again;
-      return EXIT_FAILURE;
-    }
-
-    process_file(argv[optind]);
-  }
-  catch (std::exception& err)
-  {
-    trace::error("fatal exception: {}", format_exception(err));
+  if (argc - optind != 1){
+    std::cerr << "missing required parameter\n" << try_again;
     return EXIT_FAILURE;
   }
-  catch (...)
-  {
+
+  process_file(argv[optind]);
+  }catch (std::exception& err){
+    trace::error("fatal exception: {}", format_exception(err));
+    return EXIT_FAILURE;
+  }catch (...){
     trace::error("fatal exception: (unknown exception)");
     return EXIT_FAILURE;
   }
 
   return EXIT_SUCCESS;
 }
-
-
-
